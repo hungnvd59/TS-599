76719c0f9ae028cb16d051cd19e966b2602e60f5
hungnv <hungnv@osp.vn>
2023-11-06 11:40:10 +0700

    firstcommit

---
+++ header.php
@@ -0,0 +1,174 @@
+<head>
+<meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Mạng bán TOUR DU LỊCH trực tuyến hàng đầu Việt nam | TS599</title>
+</head>
+<?php wp_head();?>
+<header>
+    <div id="vnt-header-top">
+               <div id="vnt-header">
+                  <div class="topHead">
+                     <div class="wrapper">
+                        <div class="hotlineTop">
+                           <div class="icon"><span>Hotline: <strong class="eff">1900 1177</strong></span>
+                              
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="mainHead">
+                     <div class="wrapper">
+                        <div class="wrapPoss">
+                           <div class="logo">
+                              <a href="lien-he-tour.html" title="Mạng bán TOUR DU LỊCH trực tuyến hàng đầu Việt nam">
+                              <a href="lien-he-tour.html"><img alt="Tour Du Lịch Việt" src="images/logo.png"></a>
+                           </div>
+                           <div class="header-tools">
+                              <div class="menuTop hidden-sm hidden-xs">
+                                 <ul>
+                                    <li class="nopos current">
+                                       <a href="lien-he-tour.html">Du lịch</a>
+                                       <div class="megaMenu">
+                                          <div class="grid">
+                                             <div class="col1">
+                                                <div class="menuNa">
+                                                   <ul>
+                                                      <li data-menu="menu1"><a href="tour-khuyen-mai.html">Du lịch trong nước</a></li>
+                                                      <li data-menu="menu2"><a href="du-lich-nuoc-ngoai.html">Du lịch nước ngoài</a></li>
+                                                </div>
+                                             </div>
+                                             <div class="col2">
+                                                <div class="menuFo active" data-menu="menu1">
+                                                   <div class="wrap">
+                                                      <ul>
+                                                         <li>
+                                                            <a href="du-lich-mien-bac.html">Miền Bắc</a>
+                                                            <ul>
+                                                               <li><a href="du-lich-ha-noi.html">Du lịch Hà Nội</a></li>
+                                                               <li><a href="du-lich-bac-kan.html">Du lịch Bắc Kạn</a></li>
+                                                               <li><a href="du-lich-bac-ninh.html">Du lịch Bắc Ninh</a></li>
+                                                               <li><a href="du-lich-dong-bac.html">Du lịch Đông Bắc</a></li>
+                                                               <li><a href="du-lich-dien-bien.html">Du lịch Điện Biên</a></li>
+                                                               <li><a href="du-lich-ha-long.html">Du lịch Hạ Long</a></li>
+                                                               <li><a href="du-lich-ha-giang.html">Du lịch Hà Giang</a></li>
+                                                               <li><a href="du-lich-lai-chau.html">Du lịch Lai Châu</a></li>
+                                                               <li><a href="du-lich-lang-son.html">Du lịch Lạng Sơn</a></li>
+                                                               <li><a href="du-lich-moc-chau.html">Du lịch Mộc Châu</a></li>
+                                                               <li><a href="du-lich-mai-chau.html">Du lịch Mai Châu</a></li>
+                                                               <li><a href="du-lich-thanh-hoa.html">Du lịch Thanh Hóa</a></li>
+                                                               <li><a href="du-lich-ninh-binh.html">Du lịch Ninh Bình</a></li>
+                                                               <li><a href="du-lich-phu-tho.html">Du lịch Phú Thọ</a></li>
+                                                               <li><a href="du-lich-sapa.html">Du lịch Sapa</a></li>
+                                                               <li><a href="du-lich-tay-bac.html">Du lịch Tây Bắc</a></li>
+                                                            </ul>
+                                                         </li>
+                                                         <li>
+                                                            <a href="du-lich-mien-trung.html">Miền Trung</a>
+                                                            <ul>
+                                                               <li><a href="du-lich-da-nang.html">Du lịch Đà Nẵng</a></li>
+                                                               <li><a href="du-lich-binh-thuan.html">Du lịch Bình Thuận</a></li>
+                                                               <li><a href="du-lich-buon-ma-thuot.html">Du lịch Buôn Ma Thuột</a></li>
+                                                               <li><a href="du-lich-kon-tum.html">Du lịch Kon Tum</a></li>
+                                                               <li><a href="du-lich-da-lat.html">Du lịch Đà Lạt</a></li>
+                                                               <li><a href="du-lich-dao-binh-ba.html">Du lịch Đảo Bình Ba</a></li>
+                                                               <li><a href="du-lich-dao-binh-hung.html">Du lịch Đảo Bình Hưng</a></li>
+                                                               <li><a href="du-lich-hoi-an.html">Du lịch Hội An</a></li>
+                                                               <li><a href="du-lich-trong-nuoc-phao-hoa-da-nang-festival-hue.html">Du lịch Huế</a></li>
+                                                               <li><a href="du-lich-trong-nuoc/du-lich-nha-trang.html">Du lịch Nha Trang</a></li>
+                                                               <li><a href="du-lich-ninh-chu.html">Du lịch Ninh Chữ</a></li>
+                                                               <li><a href="du-lich-ninh-thuan.html">Du lịch Ninh Thuận</a></li>
+                                                               <li><a href="du-lich-phan-thiet.html">Du lịch Phan Thiết</a></li>
+                                                               <li><a href="du-lich-phu-yen.html">Du lịch Phú Yên</a></li>
+                                                               <li><a href="du-lich-quy-nhon.html">Du lịch Quy Nhơn </a></li>
+                                                               <li><a href="du-lich-quang-binh.html">Du lịch Quảng Bình</a></li>
+                                                               <li><a href="du-lich-quang-nam.html">Du lịch Quảng Nam</a></li>
+                                                               <li><a href="du-lich-quang-ngai.html">Du lịch Quảng Ngãi</a></li>
+                                                               <li><a href="du-lich-binh-dinh.html">Du lịch Bình Định </a></li>
+                                                               <li><a href="du-lich-tay-nguyen.html">Du lịch Tây Nguyên</a></li>
+                                                            </ul>
+                                                         </li>
+                                                         <li>
+                                                            <a href="du-lich-mien-nam.html">Miền Nam</a>
+                                                            <ul>
+                                                               <li><a href="du-lich-tay-ninh.html">Du lịch Tây Ninh</a></li>
+                                                               <li><a href="du-lich-phu-quoc.html">Du lịch Phú Quốc</a></li>
+                                                               <li><a href="du-lich-an-giang.html">Du lịch An Giang</a></li>
+                                                               <li><a href="du-lich-bac-lieu.html">Du lịch Bạc Liêu</a></li>
+                                                               <li><a href="du-lich-ben-tre.html">Du lịch Bến Tre</a></li>
+                                                               <li><a href="du-lich-ca-mau.html">Du lịch Cà Mau</a></li>
+                                                               <li><a href="du-lich-can-tho.html">Du lịch Cần Thơ</a></li>
+                                                               <li><a href="du-lich-con-dao.html">Du lịch Côn Đảo</a></li>
+                                                               <li><a href="du-lich-chau-doc.html">Du lịch Châu Đốc</a></li>
+                                                               <li><a href="du-lich-dao-ba-lua.html">Du lịch Đảo Bà Lụa</a></li>
+                                                               <li><a href="du-lich-ha-tien.html">Du lịch Hà Tiên</a></li>
+                                                               <li><a href="du-lich-kien-giang.html">Du lịch Kiên Giang</a></li>
+                                                               <li><a href="du-lich-long-an.html">Du lịch Long An</a></li>
+                                                               <li><a href="du-lich-nam-du.html">Du lịch Nam Du</a></li>
+                                                               <li><a href="du-lich-mien-tay.html">Du lịch Miền Tây</a></li>
+                                                               <li><a href="du-lich-soc-trang.html">Du lịch Sóc Trăng</a></li>
+                                                               <li><a href="du-lich-tien-giang.html">Du lịch Tiền Giang</a></li>
+                                                               <li><a href="du-lich-dong-thap.html">Du lịch Đồng Tháp</a></li>
+                                                               <li><a href="du-lich-vung-tau.html">Du lịch Vũng Tàu</a></li>
+                                                            </ul>
+                                                         </li>
+                                                         <li>
+                                                          <a href="du-lich-mien-nam.html">Miền Tây</a>
+                                                          <ul>
+                                                             <li><a href="du-lich-tay-ninh.html">Du lịch Tây Ninh</a></li>
+                                                             <li><a href="du-lich-phu-quoc.html">Du lịch Phú Quốc</a></li>
+                                                             <li><a href="du-lich-an-giang.html">Du lịch An Giang</a></li>
+                                                             <li><a href="du-lich-bac-lieu.html">Du lịch Bạc Liêu</a></li>
+                                                             <li><a href="du-lich-ben-tre.html">Du lịch Bến Tre</a></li>
+                                                             <li><a href="du-lich-ca-mau.html">Du lịch Cà Mau</a></li>
+                                                             <li><a href="du-lich-can-tho.html">Du lịch Cần Thơ</a></li>
+                                                             <li><a href="du-lich-con-dao.html">Du lịch Côn Đảo</a></li>
+                                                             <li><a href="du-lich-chau-doc.html">Du lịch Châu Đốc</a></li>
+                                                             <li><a href="du-lich-dao-ba-lua.html">Du lịch Đảo Bà Lụa</a></li>
+                                                             <li><a href="du-lich-ha-tien.html">Du lịch Hà Tiên</a></li>
+                                                             <li><a href="du-lich-kien-giang.html">Du lịch Kiên Giang</a></li>
+                                                             <li><a href="du-lich-long-an.html">Du lịch Long An</a></li>
+                                                             <li><a href="du-lich-nam-du.html">Du lịch Nam Du</a></li>
+                                                             <li><a href="du-lich-mien-tay.html">Du lịch Miền Tây</a></li>
+                                                             <li><a href="du-lich-soc-trang.html">Du lịch Sóc Trăng</a></li>
+                                                             <li><a href="du-lich-tien-giang.html">Du lịch Tiền Giang</a></li>
+                                                             <li><a href="du-lich-dong-thap.html">Du lịch Đồng Tháp</a></li>
+                                                             <li><a href="du-lich-vung-tau.html">Du lịch Vũng Tàu</a></li>
+                                                          </ul>
+                                                       </li>
+                                                      </ul>
+                                                   </div>
+                                                   <div class="linkAllMenu"><a href="tour-khuyen-mai.html"><span>Tất cả tour trong nước</span></a></div>
+                                                </div>
+                                                <div class="menuFo" data-menu="menu2">
+                                                   <div class="wrap">
+                                                      <ul class="v2">
+                                                         <li>
+                                                            <a href="du-lich-chau-a.html">Du lịch Châu Á</a>
+                                                            <ul>
+                                                               <li><a href="du-lich-singapore.html">Du lịch Singapore</a></li>
+                                                               <li><a href="du-lich-thai-lan.html">Du lịch Thái Lan</a></li>
+                                                            </ul>
+                                                         </li>
+                                                      </ul>
+                                                   </div>
+                                                   <div class="linkAllMenu"><a href="du-lich-nuoc-ngoai.html"><span>Tất cả tour nước ngoài </span></a></div>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       </div>
+                                    </li>
+                                    <li class><a href="khach-san/index.html">Khách sạn</a></li>
+                                    <li class><a href="ve-may-bay.html">Vé</a></li>
+                                    <li class><a href="xe-du-lich.html">Thuê xe</a></li>
+                                    <li class>
+                                       <a href="tin-tuc/gioi-thieu.html">Giới thiệu</a>
+                                    </li>
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+</header>
